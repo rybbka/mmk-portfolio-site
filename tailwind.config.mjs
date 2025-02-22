662d02513e917d84e5ee54d83a0075063b76bfcc
Majkel Kokocinski <majkelkokocinski@gmail.com>
2025-02-22 17:18:46 +0100

    Fix CSS configuration and styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,8 +2,8 @@
 const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
