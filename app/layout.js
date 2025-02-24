3213945de85c3c5f5b44cd5fbb6c9883dc8b2fde
Majkel Kokocinski <majkelkokocinski@gmail.com>
2025-02-24 20:47:12 +0100

    Add Vercel configuration for CSS handling

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full">
-      <head>
-        <link rel="stylesheet" href="/_next/static/css/app.css" precedence="default" />
-      </head>
-      <body className="antialiased font-['Suisse_Intl_Mono'] font-bold bg-white h-full">
+    <html lang="en">
+      <body className="antialiased font-['Suisse_Intl_Mono'] font-bold bg-white">
         {children}
       </body>
     </html>
