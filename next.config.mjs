564dc79d79549b919e82c671235a40f1cfaec6f4
Majkel Kokocinski <majkelkokocinski@gmail.com>
2025-02-24 21:58:13 +0100

    Revert webpack changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,6 @@ const config = {
     formats: ['image/avif', 'image/webp'],
   },
   output: 'standalone',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader']
-    });
-    return config;
-  }
 };
 
 export default config;
