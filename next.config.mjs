d43e23ea0abba7777f7a6d84edcb44877cb52acd
Majkel Kokocinski <majkelkokocinski@gmail.com>
2025-02-24 21:55:50 +0100

    Add webpack CSS handling for production build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const config = {
     formats: ['image/avif', 'image/webp'],
   },
   output: 'standalone',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader']
+    });
+    return config;
+  }
 };
 
 export default config;
